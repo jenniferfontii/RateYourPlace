d921ea65d013b536cea72306102a2dd21675cc44
Jennifer <105367486+jenniferfontii@users.noreply.github.com>
2025-02-04 14:00:12 +0000

    #2 - welcome page redirects to correct pages

--- app/src/main/java/com/example/rateyourplace/MainActivity.java
+++ app/src/main/java/com/example/rateyourplace/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.rateyourplace;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -19,6 +22,34 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        Button loginBtn = findViewById(R.id.loginBtn);
+        Button signupBtn = findViewById(R.id.signupBtn);
+        Button noAccountBtn = findViewById(R.id.noAccountBtn);
+
+       loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, login.class);
+                startActivity(intent);
+            }
+        });
+
+        signupBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, signup.class);
+                startActivity(intent);
+            }
+        });
+
+        noAccountBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, home.class);
+                startActivity(intent);
+            }
         });
     }
 }
