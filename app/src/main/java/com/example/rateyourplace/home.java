6ee8d4ebcd6cbc8274b522d2c3ed5664bf4065c1
Jennifer <105367486+jenniferfontii@users.noreply.github.com>
2025-02-04 13:41:15 +0000

    #1 - frontend, added bottom menu and drawables, search bar still not working

--- app/src/main/java/com/example/rateyourplace/home.java
+++ app/src/main/java/com/example/rateyourplace/home.java
@@ -7,6 +7,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.appcompat.widget.SearchView;
 
 public class home extends AppCompatActivity {
 
