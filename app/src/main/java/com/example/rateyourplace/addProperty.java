5ce8d375953d464141cb085b12381e2be982560c
Jennifer <105367486+jenniferfontii@users.noreply.github.com>
2025-02-19 09:33:08 +0000

    #3 - added add property action listener and created add property activity

--- app/src/main/java/com/example/rateyourplace/addProperty.java
+++ app/src/main/java/com/example/rateyourplace/addProperty.java
@@ -1,38 +1,24 @@
 package com.example.rateyourplace;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
-import androidx.appcompat.widget.SearchView;
 
-public class home extends AppCompatActivity {
+public class addProperty extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_home);
+        setContentView(R.layout.activity_add_property);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
-        });
-
-        Button addPropertyBtn = findViewById(R.id.addPropertyBtn);
-
-        addPropertyBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(home.this, addProperty.class);
-                startActivity(intent);
-            }
         });
     }
 }
